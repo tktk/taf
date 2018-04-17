0dfce7366e0d06fbd91ab1e2f86185f6c88eb64e
tekuto <g.tekuto@gmail.com>
2018-04-17 14:39:11 +0900

    モジュールオプションを追加

--- options.py
+++ options.py
@@ -1,8 +1,43 @@
 # -*- coding: utf-8 -*-
 
 from . import common
+from . import taf
+
+from pydoc import ModuleScanner
 
 def options(
     _context,
 ):
+    _addModuleOptions( _context )
+
     common.loadTools( _context )
+
+def _addModuleOptions(
+    _context,
+):
+    for module in _getModuleNames():
+        _context.add_option(
+            _optionModuleKey( module ),
+            action = 'store_true',
+            default = False,
+        )
+
+def _getModuleNames(
+):
+    TSCRIPTS_PATH = taf.TSCRIPTS_DIR + '.'
+    TSCRIPTS_PREFIX = TSCRIPTS_PATH + taf.PACKAGE_NAME
+
+    modules = []
+    ModuleScanner().run(
+        lambda *args : modules.append( args[ 1 ] ),
+        key = TSCRIPTS_PREFIX,
+    )
+
+    LENGTH = len( TSCRIPTS_PATH )
+
+    return [ module[ LENGTH: ] for module in modules ]
+
+def _optionModuleKey(
+    _MODULE,
+):
+    return '--' + taf.MODULE_OPTION_PREFIX + '.' + _MODULE
--- taf.py
+++ taf.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
+PACKAGE_NAME = None
+
 LOAD_TOOLS = None
+
+TSCRIPTS_DIR = 'tscripts'
+MODULE_OPTION_PREFIX = 'enable'
