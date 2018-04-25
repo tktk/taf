2dcb9ab953d09bcb834ecf6861cb482756e937df
tekuto <g.tekuto@gmail.com>
2018-04-26 01:36:29 +0900

    buildの処理を修正

--- taf/build.py
+++ taf/build.py
@@ -4,7 +4,6 @@ from . import taf
 from . import module
 
 import importlib
-import os.path
 
 def build(
     _context,
@@ -14,96 +13,10 @@ def build(
 
         importlib.import_module( taf.TSCRIPTS_DIR + '.' + moduleName )
 
-        SOURCE = _initializeSource()
-
         module.BUILDER(
             _context,
             module.TARGET,
-            SOURCE,
+            module.SOURCE,
             module.LIB,
             module.USE,
         )
-
-def _initializeSource(
-):
-    source = []
-
-    _generateSourceList(
-        source,
-        module.SOURCE,
-        os.path.join(
-            taf.SRC_DIR,
-            taf.PACKAGE_NAME,
-        ),
-    )
-
-    source.sort()
-
-    return source
-
-def _generateSourceList(
-    _sourceList,
-    _SOURCE,
-    _PARENT,
-):
-    TYPE = type( _SOURCE )
-
-    if TYPE is dict:
-        _generateSourceListForDict(
-            _sourceList,
-            _SOURCE,
-            _PARENT,
-        )
-    elif TYPE is list:
-        _generateSourceListForList(
-            _sourceList,
-            _SOURCE,
-            _PARENT,
-        )
-    else:
-        _generateSourceListForString(
-            _sourceList,
-            _SOURCE,
-            _PARENT,
-        )
-
-def _generateSourceListForDict(
-    _sourceList,
-    _SOURCE_DICT,
-    _PARENT,
-):
-    for DIR, SOURCE in _SOURCE_DICT.items():
-        PARENT = os.path.join(
-            _PARENT,
-            DIR,
-        )
-
-        _generateSourceList(
-            _sourceList,
-            SOURCE,
-            PARENT,
-        )
-
-def _generateSourceListForList(
-    _sourceList,
-    _SOURCE_LIST,
-    _PARENT,
-):
-    for SOURCE in _SOURCE_LIST:
-        _generateSourceList(
-            _sourceList,
-            SOURCE,
-            _PARENT,
-        )
-
-def _generateSourceListForString(
-    _sourceList,
-    _SOURCE_STRING,
-    _PARENT,
-):
-    _sourceList.append(
-        os.path.join(
-            _PARENT,
-            _SOURCE_STRING,
-        )
-    )
